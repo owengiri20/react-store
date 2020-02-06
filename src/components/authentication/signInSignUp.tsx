2e9edca0d5e2049c583b1b6d4af73800bc949b92
owengiri20 <owengiri7@gmail.com>
2020-02-06 23:08:38 +0800

    start auth pages

---
+++ src/components/authentication/signInSignUp.tsx
@@ -0,0 +1,16 @@
+import React from "react"
+import "../../styles/signInSignUp.styles.scss"
+
+interface Props {}
+
+const SignInSignUp = (props: Props) => {
+	const {} = props
+
+	return (
+		<div>
+			<h1>authS</h1>
+		</div>
+	)
+}
+
+export default SignInSignUp
